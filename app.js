01e7d551c4d98761bb7e4434ccfa82da1bcd94dd
DongQuYiPiLang <jswjqch@gmail.com>
2017-03-24 07:38:16 +0000

    change mongodb from local to mongolab

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     authRoutes          = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp");
-
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://mike:11111@ds141410.mlab.com:41410/yelp_camp");
+//mongodb://mike:11111@ds141410.mlab.com:41410/yelp_camp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
